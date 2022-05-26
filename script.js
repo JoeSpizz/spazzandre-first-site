f86986d96e2a569c1a2117e667f480c7076681f0
Joe Spizzandre <spizjo01@gmail.com>
2022-05-25 20:15:30 -0600

    added and connected script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+const h2 = document.createElement("h2");
+h2.textContent = "This content was added by JavaScript, which seems inefficient but is neat!";
+
+document.querySelector("body").appendChild(h2);
