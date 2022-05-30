2769c1c94d4f2fec065c18a982124da0d61057be
Joe <spizjo01@gmail.com>
2022-05-30 17:51:24 -0600

    Added lots of styling and color changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content was added by JavaScript, which seems inefficient but is neat!";
 
-document.querySelector("body").appendChild(h2);
+
 
